178d465789857a0c8b2e9e080ac81b134cd45e3c
1135975331 <dkswnstj021@gmail.com>
2023-09-06 16:15:10 +0900

    Solved: Baekjoon - 1316

---
+++ ProgrammingTest_CSharp/Baekjoon1316.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ProgrammingTest;
+
+public class Baekjoon1316
+{
+    public static void Baekjoon1316Main()
+    {
+        var strAmount      = int.Parse(Console.ReadLine() ?? "0");
+        var groupWordCount = 0;
+
+        for(var i = 0; i < strAmount; i++) {
+            var str = Console.ReadLine() ?? "0";
+            groupWordCount += GetIsGroupWord(str) ? 1 : 0;
+        }
+        
+        Console.WriteLine(groupWordCount);
+    }
+
+    public static bool GetIsGroupWord(string str)
+    {
+        for(var a = 0; a < str.Length; a++) {
+            var selectedChar = str[a];
+	                
+            var prevIndex = a;
+            for(var b = a+1; b < str.Length; b++) {
+                if(selectedChar != str[b])
+                    continue;
+
+                if(b == prevIndex + 1)
+                    prevIndex = b;
+                else
+                    return false;
+            }
+            
+            a = prevIndex;
+        }
+        
+        return true;
+    }
+}
